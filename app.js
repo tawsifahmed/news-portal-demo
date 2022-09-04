e256b227f3446108a9172f9a5992116f101f8307
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2022-09-04 23:16:19 +0600

    successfully added onclick handler on dynamic category button

--- app.js
+++ app.js
@@ -15,19 +15,69 @@ const loadCategoryFetch = () => {
         .catch(error => console.log(error))
 }
 
+// const newsDetailsFetch = () => {
+//     const url1 = `https://openapi.programming-hero.com/api/news/category/01`;
+//     fetch(url1)
+//         .then(res1 => res1.json())
+//         .then(data1 => displayNews(data1.data))
+//         .catch(error => console.log(error))
+// }
+
 const displayCategories = categories => {
-    console.log(categories);
+    // console.log(categories);
     const categoriesContainer = document.getElementById('categories-container');
     categories.forEach(category => {
         const categoryDiv = document.createElement('a');
         categoryDiv.classList.add('d-inline');
-
+        // console.log(category);
         categoryDiv.innerHTML = `
         
-        <span class="justify-content-center p-4">${category.category_name}</span >
+        <button onclick="loadNews('${category.category_id}')" class="justify-content-center p-4 btn btn-link">${category.category_name}</button >
             `;
         categoriesContainer.appendChild(categoryDiv);
     })
 }
 
+const loadNews = id => {
+    const url2 = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    console.log(url2);
+    fetch(url2)
+        .then(res2 => res2.json())
+        .then(data2 => displayNews(data2.data))
+        .catch(error => console.log(error))
+
+}
+
+const displayNews = newss => {
+    console.log(newss);
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = ''
+    newss.forEach(news => {
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col');
+        newsDiv.innerHTML = `
+        <div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${news.image_url}" class="img-fluid rounded-start w-100 h-100" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${news.title}</h5>
+        <p class="card-text">${news.details}</p>
+        <img src="">
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+        `;
+        newsContainer.appendChild(newsDiv);
+    })
+}
+
+// loadNews();
+
 loadCategoryFetch();
+
+newsDetailsFetch();
