dcfb1cfeb650c0d10b6cb07e8c8a4db4f7f78442
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2022-09-04 15:49:58 +0600

    created a page for blog section

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById('blog-section').addEventListener('click', function () {
+    window.location.href = 'faq.html';
+});
+
+document.getElementById('news-section').addEventListener('click', function () {
+    window.location.href = 'index.html';
+});
+
